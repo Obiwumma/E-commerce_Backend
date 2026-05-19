752fa35f49442af5fa2d89d3ecabf9393f27d9ec
Obiwumma Pascal <pobiwumma@gmail.com>
2026-05-19 16:45:36 +0100

    Backend connection string fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,5 +4,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 
 // We export this 'db' instance so ANY file in our project can import and use it
 const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
+const client = postgres(connectionString, {
+  max: 10,               // Do not open more than 10 simultaneous phone lines
+  idle_timeout: 20,      // If a line is quiet for 20 seconds, Express will hang it up cleanly itself
+  max_lifetime: 60 * 30, // Force a completely fresh connection every 30 minutes
+});
 export const db = drizzle(client);
